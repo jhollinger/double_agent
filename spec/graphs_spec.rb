336f4191e324e5f4a48f58cdb35be7d85b7e5255
Jordan Hollinger <jordan.hollinger@gmail.com>
2012-07-18 23:59:42 -0400

    Add graphing test

---
+++ spec/graphs_spec.rb
@@ -0,0 +1,60 @@
+require File.dirname(__FILE__) + '/spec_helper'
+begin
+  require File.dirname(__FILE__) + '/../lib/double_agent/graphs'
+  require 'digest/md5'
+rescue LoadError
+  $stderr.puts "NOTICE \"gruff\" is not available; graphing module tests will be skipped"
+end
+
+if defined? Gruff
+  log_glob = File.dirname(__FILE__) + '/data/*.access.log*'
+  entries = DoubleAgent::Logs::entries(log_glob, :match => /^\d/)
+
+  describe DoubleAgent do
+    context 'Graphs' do
+      before :each do
+        @file_path = '/tmp/double_agent_graph_test.png'
+        @md5 = ->(path) { Digest::MD5.hexdigest(File.read(path)) }
+      end
+
+      after :each do
+        File.unlink(@file_path) if File.exists? @file_path
+      end
+
+      it 'should write a simple pie chart' do
+        DoubleAgent::Graphs.pie(entries, :browser_family, @file_path, 'Browser Share')
+        @md5[@file_path].should == 'c58e8724efe6f94e421e78a19fd51c6b'
+      end
+
+      it 'should write a complex pie chart' do
+        DoubleAgent::Graphs.pie(entries, :browser_family, @file_path) do |pie|
+          pie.title = 'Browser Share'
+        end
+        @md5[@file_path].should == 'c58e8724efe6f94e421e78a19fd51c6b'
+      end
+
+      it 'should write a simple multiline graph' do
+        DoubleAgent::Graphs.line(entries, :browser_family, :date, @file_path, 'Browser Share')
+        @md5[@file_path].should == '4eddcafdbdd6b2936bfaade1b72990a6'
+      end
+
+      it 'should write a formatted line graph' do
+        DoubleAgent::Graphs.line(entries, :browser_family, :date, @file_path, 'Browser Share') do |chart, labeler|
+          chart.theme = chart.theme_odeo
+
+          labeler.call(2) do |date|
+            date.strftime('%m/%d/%Y')
+          end
+        end
+        @md5[@file_path].should == '9e05efd35cb8ff361dfc20ee6a0d62e0'
+      end
+
+      it 'should write a monthly line graph' do
+        DoubleAgent::Graphs.line(entries, :browser_family, ->(e) { e.date.strftime('%m/%Y') }, @file_path, 'Browser Share') do |chart, labeler|
+          chart.theme = chart.theme_odeo
+        end
+        @md5[@file_path].should == '263562b4e4eacbe283958116c7072c0d'
+      end
+    end
+  end
+end
